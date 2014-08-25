c424d0ff6d4e66d0e70f38f34a9a321bf84d5cce
sole <sole@mozilla.com>
2014-08-25 20:32:43 +0200

    rearrange main.js in a way that doesnt give me the creeps

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,54 @@
-;(function(){
-  var file = document.querySelector('input[type=file]')
-  var submitBtn = document.querySelector('button[type=submit]')
-  var img = document.createElement('img')
+window.addEventListener('DOMContentLoaded', function() {
+  var file = document.querySelector('input[type=file]');
+  var submitBtn = document.querySelector('button[type=submit]');
+  var img = document.createElement('img');
+  document.querySelector('header').appendChild(img);
   var payload = {
     content: {},
     meta: {}
-  }
+  };
 
-  var submitHandler = function(e){
-    e.preventDefault()
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function(){
-      var parsed
-      if (xhr.readyState === 4) {
-        parsed = JSON.parse(xhr.responseText)
-        img.src = parsed.content.data
-      }
-    }
-    xhr.open('POST', '/service')
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    if (payload.content.data) {
-      xhr.send(JSON.stringify(payload))
-    }
-  }
 
-  var fileHandler = function(e) {
-    var reader = new FileReader()
+  function fileHandler(e) {
+
+    var reader = new FileReader();
     reader.onloadend = function() {
-      document.querySelector('header').appendChild(img)
-      img.src = reader.result
-      payload.content.type = e.target.files[0].type
-      payload.content.data = reader.result
-    }
+      img.src = reader.result;
+      payload.content.type = e.target.files[0].type;
+      payload.content.data = reader.result;
+    };
 
     if (e.target.files[0].type.indexOf('image') !== 0) {
-      alert('Nah, this is for images')
+      alert('Nah, this is for images');
     } else {
-      reader.readAsDataURL(e.target.files[0])
+      reader.readAsDataURL(e.target.files[0]);
     }
+    
   }
 
-  submitBtn.addEventListener('click', submitHandler)
-  file.addEventListener('change', fileHandler)
-})()
+
+  function submitHandler(e){
+
+    e.preventDefault();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      var parsed;
+      if (xhr.readyState === 4) {
+        parsed = JSON.parse(xhr.responseText);
+        img.src = parsed.content.data;
+      }
+    };
+
+    xhr.open('POST', '/service');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    if (payload.content.data) {
+      xhr.send(JSON.stringify(payload));
+    }
+
+  }
+
+
+  file.addEventListener('change', fileHandler);
+  submitBtn.addEventListener('click', submitHandler);
+
+});
